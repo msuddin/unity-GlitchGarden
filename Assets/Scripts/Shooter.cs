d38e2bfcdcca52eb7f8c6e7caed75922ef09faa1
msuddin <mo@thetestroom.com>
2020-05-10 18:20:06 +0100

    Added Gnome and Gravestone defenders

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -5,6 +5,55 @@ using UnityEngine;
 public class Shooter : MonoBehaviour
 {
     [SerializeField] GameObject projectile, gun;
+    AttackerSpawner myLaneSpawner;
+    Animator animator;
+
+    private void Start()
+    {
+        SetLanceSpawner();
+        animator = FindObjectOfType<Animator>();
+    }
+
+    private void Update()
+    {
+        if (IsAttackerInLane())
+        {
+            Debug.Log("Fire away");
+            animator.SetBool("IsAttacking", true);
+        }
+        else
+        {
+            Debug.Log("Sit and wait");
+            animator.SetBool("IsAttacking", false);
+        }
+    }
+
+    private void SetLanceSpawner()
+    {
+        AttackerSpawner[] spawners = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner spawner in spawners)
+        {
+            bool isCloseEnough = (Mathf.Abs(spawner.transform.position.y - transform.position.y) <= Mathf.Epsilon);
+
+            if (isCloseEnough)
+            {
+                myLaneSpawner = spawner;
+            }
+        }
+    }
+
+    private bool IsAttackerInLane()
+    {
+        if (myLaneSpawner.transform.childCount <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
 
     public void Fire()
     {
